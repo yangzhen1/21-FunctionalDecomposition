210b405aaebe13d4686da39ad384bc1cfa0bb8f0
DavidMutchler <mutchler@rose-hulman.edu>
2019-01-24 06:54:00 -0500

    Initial version from your instructor.

---
+++ README.md
@@ -0,0 +1,5 @@
+# FunctionalDecompostion
+A larger problem to help you learn:
+  -- Iterative Enhancement
+  -- Functional Decomposition
+  -- Pair Programming
---
+++ src/m1_hangman.py
@@ -0,0 +1,10 @@
+"""
+Hangman.
+
+Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+
+####### Do NOT attempt this assignment before class! #######
+
